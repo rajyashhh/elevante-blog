78957010e4a6cd2e2900e6d6cdadc015236f4e31
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-24 02:00:53 +0530

    used useState hook and added css

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
+
+
 const Home = () => {
+
+    
+    const [name, setName] = useState('Ogilvy');
+    const [age, setAge] = useState(25);
+    function handleClick(){
+        setName('Yash');
+        setAge(19);
+    }
+    
+
+    
     return ( 
         <div className="home">
-            Homepage
+            <p>{name} is {age} years old!</p>
+            <h2>Homepage</h2>
+            <button onClick={handleClick}>CLick me!</button>
+            
         </div>
      );
 }
