f50d16c468203d3d65b12b9f063ab95bbc1eb41f
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-24 14:36:24 +0530

    added delete functionality

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,20 @@ import BlogList from './bloglist';
 const Home = () => {    
 
     const[blogs, setBlogs] = useState([
-        {title : 'My new website', body: 'lorem ipsum...', author: 'ronny', id: 1},
+        {title : 'My new website', body: 'lorem ipsum...', author: 'rahul', id: 1},
         {title : 'DEV vs DSA', body: 'lorem ipsum...', author: 'sunny', id: 2},
         {title : 'What not to do in Engineering!', body: 'lorem ipsum...', author: 'moi', id: 3},
     ])
     
-
+    function handleDelete(id){
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs"/>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+            
         </div>
      );
 }
