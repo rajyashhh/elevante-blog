96a0148472343ae50d6a1d39e29bbb03403edf1f
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-29 03:49:45 +0530

    error management in react

--- src/Home.js
+++ src/Home.js
@@ -9,27 +9,37 @@ const Home = () => {
 
     const[blogs, setBlogs] = useState(null);
     const[isPending, setIsPending] = useState(true);
-
+    const[error, setError] = useState(null);
     
     
     
     useEffect(()=>{ setTimeout(
         ()=>{
-
-        fetch('http://localhost:8000/blogs').then(res => {
+        
+        fetch('http://localhost:8000/blofgs').then(res => {
+            console.log(res);
+            if(!res.ok){
+                throw Error('Could not fetch the data for that resource')
+            }
             return res.json();
         })
-        .then((data)=>{
-            console.log(data)
+            .then((data)=>{
+            
             setBlogs(data);
             setIsPending(false);
-        });
+            setError(null);
+        })
+        .catch(err=>{
+            setError(err.message);
+            setIsPending(false);
+        })
         }, 1000
     )
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
             
