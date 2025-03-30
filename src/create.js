9a3a9cb59fea6d7ea38027af954b77d473effe3b
Yash Raj Singh <workmail.yrs@gmail.com>
2025-03-30 16:43:48 +0530

    added a flow

--- src/create.js
+++ src/create.js
@@ -1,13 +1,35 @@
+
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Create = () => {
     const [title, setTitle] = useState(" ");
     const [body, setBody] = useState(" ");
     const [author, setAuthor] = useState("yash");
+    const [isPending, setIsPending] = useState(false);
+    const navigate = useNavigate();
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const blog = {title, body, author};
+        setIsPending(true);
+        fetch('http://localhost:2000/blogs',{
+            method: 'POST',
+            headers : { "Content-Type":"application/json"},
+            body: JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added')
+            setIsPending(false)
+            navigate('/')
+        })
+
+       
+        
+    }
     return ( 
         <div className="create">
             <h2>Create a Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog Title</label>
                 <input 
                     type = "text"
@@ -30,7 +52,8 @@ const Create = () => {
                 <option value="yash">Yash</option>
                 <option value="rahul">Rahul</option>
                </select>
-               <button>Add Blog</button>
+               {!isPending && <button>Add Blog</button>}
+               {isPending && <button disabled>Adding Blog...</button>}
                <p>{title}</p>
                <p>{body}</p>
                <p>{author}</p>
